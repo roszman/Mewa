848db1d97edc8f591d1218ac811c891607ca5403
roszman <roszman@gmail.com>
2015-05-19 20:25:55 +0200

    mewa sln + empty CacheService project

---
+++ Mewa.CacheService/CacheService.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Linq;
+using System.ServiceProcess;
+using System.Text;
+
+namespace Mewa.CacheService
+{
+    public partial class CacheService : ServiceBase
+    {
+        public CacheService()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnStart(string[] args)
+        {
+        }
+
+        protected override void OnStop()
+        {
+        }
+    }
+}
