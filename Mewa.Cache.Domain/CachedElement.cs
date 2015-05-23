b83a4e4efd7c1b5a5c480676e84f85606f4e04ae
roszman <roszman@gmail.com>
2015-05-23 11:35:51 +0200

    add Domain project

---
+++ Mewa.Cache.Domain/CachedElement.cs
@@ -0,0 +1,11 @@
+﻿namespace Mewa.Cache.Domain
+{
+    public class CachedElement
+    {
+        public long Id { get; set; }
+        public string Url { get; set; }
+        public string TagName { get; set; }
+        public string TagValue { get; set; }
+        public string Value { get; set; }
+    }
+}
