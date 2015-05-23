48bd25296d7783415161adf3c9882def341c2d20
roszman <roszman@gmail.com>
2015-05-23 11:37:09 +0200

    add Infrastructure with CachedElement repository

---
+++ Mewa.Cache.Infrastructure/Repository/CacheRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Mewa.Cache.Domain;
+
+namespace Mewa.Cache.Infrastructure.Repository
+{
+    public class CacheRepository : ICacheRepository
+    {
+        public IEnumerable<CachedElement> GetElements()
+        {
+            return new List<CachedElement>
+            {
+                new CachedElement
+                {
+                    TagName = "tag name",
+                    TagValue = "tag value",
+                    Url = "url",
+                    Value = "<span>Follow us on<a href='https://jabbr.net/#/rooms/Lobby' class='jabbr' title='JabbR'></a><a href='https://www.facebook.com/asp.net' class='facebook' title='Facebook'></a><a href='http://twitter.com/aspnet' class='twitter' title='Twitter'></a></span>"
+                },
+                new CachedElement
+                {
+                    TagName = "tag name",
+                    TagValue = "tag value",
+                    Url = "url",
+                    Value = "<span>Follow us on<a href='https://jabbr.net/#/rooms/Lobby' class='jabbr' title='JabbR'></a><a href='https://www.facebook.com/asp.net' class='facebook' title='Facebook'></a><a href='http://twitter.com/aspnet' class='twitter' title='Twitter'></a></span>"
+                }
+
+            };
+        }
+    }
+}
---
+++ Mewa.Cache.Infrastructure/Repository/ICacheRepository.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+using Mewa.Cache.Domain;
+
+namespace Mewa.Cache.Infrastructure.Repository
+{
+    public interface ICacheRepository
+    {
+        IEnumerable<CachedElement> GetElements();
+    }
+}
