efa6ecba70f071b11516a8baa0fd83dfa995b9d1
roszman <roszman@gmail.com>
2015-05-23 16:06:40 +0200

    CacheRepoistory using  database

--- Mewa.Cache.Infrastructure/Repository/CacheRepository.cs
+++ Mewa.Cache.Infrastructure/Repository/CacheRepository.cs
@@ -17,26 +17,8 @@ namespace Mewa.Cache.Infrastructure.Repository
 
         public IEnumerable<CachedElement> GetElements()
         {
-
-            //return _session.QueryOver<CachedElement>().T;
-            return new List<CachedElement>
-            {
-                new CachedElement
-                {
-                    TagName = "tag name",
-                    TagValue = "tag value",
-                    Url = "url",
-                    Value = "<span>Follow us on<a href='https://jabbr.net/#/rooms/Lobby' class='jabbr' title='JabbR'></a><a href='https://www.facebook.com/asp.net' class='facebook' title='Facebook'></a><a href='http://twitter.com/aspnet' class='twitter' title='Twitter'></a></span>"
-                },
-                new CachedElement
-                {
-                    TagName = "tag name",
-                    TagValue = "tag value",
-                    Url = "url",
-                    Value = "<span>Follow us on<a href='https://jabbr.net/#/rooms/Lobby' class='jabbr' title='JabbR'></a><a href='https://www.facebook.com/asp.net' class='facebook' title='Facebook'></a><a href='http://twitter.com/aspnet' class='twitter' title='Twitter'></a></span>"
-                }
-
-            };
+            //TODO error handling
+            return _session.QueryOver<CachedElement>().List<CachedElement>();
         }
     }
 }
