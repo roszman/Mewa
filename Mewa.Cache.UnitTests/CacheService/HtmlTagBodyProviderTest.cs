86eaa3dbad1207752da7eaa896da8104b8454249
roszman <roszman@gmail.com>
2015-05-24 16:10:52 +0200

    fixed tests

--- Mewa.Cache.UnitTests/CacheService/HtmlTagBodyProviderTest.cs
+++ Mewa.Cache.UnitTests/CacheService/HtmlTagBodyProviderTest.cs
@@ -1,4 +1,4 @@
-﻿using Mewa.CacheService;
+﻿using Mewa.Cache.WindowsServiceHost;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace Mewa.Cache.UnitTests.CacheService
@@ -10,13 +10,11 @@ namespace Mewa.Cache.UnitTests.CacheService
         public void GetTagValue_ValidTripleConfig_ReturnHtmlTagValue()
         {
             //TODO take html from the local resource
-            var webCrawler = new HtmlStringProvider();
             //<div id="bakgrunnsbilde">
-            var htmlString = webCrawler.GetHtmlString(
+            var htmlString = HtmlStringProvider.GetHtmlString(
                     "http://splash.seagull.no/splash/");
             
-            var tagBodyProvider = new HtmlTagBodyProvider();
-            var htmlTag = tagBodyProvider.GetTagBodyFromHtml(htmlString, "div", "id", "bakgrunnsbilde");
+            var htmlTag = HtmlTagBodyProvider.GetTagBodyFromHtml(htmlString, "div", "id", "bakgrunnsbilde");
             
             Assert.AreEqual(@"<img src=""img/Bakgrunnsbilde_520.png"" width=""520"" height=""340"" alt=""Bakgrunnsbilde"">", htmlTag);
         }
