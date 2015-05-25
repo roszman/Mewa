e1ab717562cb0d382986881844727eb67fcb8750
roszman <roszman@gmail.com>
2015-05-25 20:57:43 +0200

    WebUI.Admin add view model

---
+++ Mewa.Cache.WebUI.Admin/Models/HtmlElelmentToCache.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mewa.Cache.WebUI.Admin.Models
+{
+    public class HtmlElelmentToCache
+    {
+        [Required]
+        public string Url { get; set; }
+        [Required]
+        public string TagName { get; set; }
+        [Required]
+        public string TagAttributeName { get; set; }
+        [Required]
+        public string TagAttributeValue { get; set; }
+    }
+}
