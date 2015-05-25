52b1b711cc081d4b16d596438ec91d25edf9cfe4
roszman <roszman@gmail.com>
2015-05-25 20:22:49 +0200

    change service contract to return some CzchedElements properties as xml attirbutes

--- Mewa.Cache.WebServiceHost/ServiceContract/CachedElement.cs
+++ Mewa.Cache.WebServiceHost/ServiceContract/CachedElement.cs
@@ -1,12 +1,15 @@
 ﻿using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Xml;
+using System.Xml.Schema;
+using System.Xml.Serialization;
 
 namespace Mewa.Cache.WebServiceHost.ServiceContract
 {
 
     // since domain object corresponding to CachedElement will not have any behaviour, i wil use CachedElement as domain object. 
     //TODO shoul it be in different assembly?
-    [DataContract]
-    public class CachedElement
+    public class CachedElement : IXmlSerializable
     {
         //TODO remove constructor ?
         public CachedElement(
@@ -27,16 +30,24 @@ namespace Mewa.Cache.WebServiceHost.ServiceContract
         {
         }
 
-        [DataMember]
         public string Url { get; set; }
-        [DataMember]
         public string TagName { get; set; }
 
         public string TagAttributeName { get; set; }
 
-        [DataMember]
         public string TagAttributeValue { get; set; }
-        [DataMember]
         public string InnerHtml { get; set; }
+
+        public XmlSchema GetSchema() { return null; }
+        public void ReadXml(XmlReader reader) { }
+
+        public void WriteXml(XmlWriter writer)
+        {
+            writer.WriteAttributeString("url", Url);
+            writer.WriteAttributeString("tag_name", TagName);
+            writer.WriteAttributeString("attribute_name", TagAttributeName);
+            writer.WriteAttributeString("attribute_value", TagAttributeValue);
+            writer.WriteElementString("innerHtml", InnerHtml);
+        }
     }
 }
--- Mewa.Cache.WebServiceHost/ServiceContract/ICache.cs
+++ Mewa.Cache.WebServiceHost/ServiceContract/ICache.cs
@@ -3,7 +3,6 @@ using System.ServiceModel;
 
 namespace Mewa.Cache.WebServiceHost.ServiceContract
 {
-    // Since it is simple service contract i will keep it in ServiceHost app, in more complicated solutions I will consider extracting it to different project
     [ServiceContract]
     public interface ICache
     {
